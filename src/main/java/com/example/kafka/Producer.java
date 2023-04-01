91cf37787196791f4d24beb56950cd56e2c0469a
jayakhan <jayakhan04@gmail.com>
2023-04-01 18:28:43 -0400

    Added Producer class

--- src/main/java/com/example/kafka/Producer.java
+++ src/main/java/com/example/kafka/Producer.java
@@ -1,7 +1,32 @@
 package com.example.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class Producer {
     public static void main(String[] args){
-        System.out.println("Hello Kafka");
+
+        //Create properties object for Producer
+        Properties prop = new Properties();
+        prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        prop.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //Create the Producer
+        final KafkaProducer<String, String> producer = new KafkaProducer<String, String>(prop);
+
+        //Create the ProducerRecord
+        ProducerRecord<String, String> record = new ProducerRecord<>("test","key2","value2");
+
+        //Send Data - Asynchronous
+        producer.send(record);
+
+        //flush and close Producer
+        producer.flush();
+        producer.close();
     }
 }
